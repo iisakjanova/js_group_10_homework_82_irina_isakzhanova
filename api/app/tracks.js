3b40d68030e66009bf42c4b6c57148d57b115a5c
Irina Isakzhanova <irina.isakjanova@gmail.com>
2021-10-31 23:13:26 +0600

    Implemented getting tracks

--- api/app/tracks.js
+++ api/app/tracks.js
@@ -1,4 +1,21 @@
 const express = require('express');
+const Track = require("../models/Track");
+
 const router = express.Router();
+
+router.get('/', async (req, res) => {
+    try {
+        const query = {};
+
+        if(req.query.album) {
+            query.album = req.query.album;
+        }
+
+        const tracks = await Track.find(query).populate('album', 'title');
+        res.send(tracks);
+    } catch (e) {
+        res.sendStatus(500);
+    }
+});
 
 module.exports = router;
